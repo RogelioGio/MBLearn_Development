a35e5ea98d441f32a67b1fab90f6d55e685ea572
Jericho Ilanga <ilangajericho@gmail.com>
2025-03-19 11:36:43 +0800

    fixed from name to id

--- app/Http/Requests/StoreCourseRequest.php
+++ app/Http/Requests/StoreCourseRequest.php
@@ -26,9 +26,9 @@ class StoreCourseRequest extends FormRequest
             'name' => 'required|unique:courses,name',
             'CourseID' => 'required|unique:courses,CourseID',
             'description' => 'required',
-            'type_id' => 'required|exists:types,type_name',
-            'category_id'=> 'required|exists:categories,category_name',
-            'training_mode_id' => 'required|exists:training__modes,mode_name',
+            'type_id' => 'required|exists:types,type_id',
+            'category_id'=> 'required|exists:categories,category_id',
+            'training_mode_id' => 'required|exists:training__modes,mode_id',
             'training_type' => 'required',
             'archived' => 'required',
         ];
