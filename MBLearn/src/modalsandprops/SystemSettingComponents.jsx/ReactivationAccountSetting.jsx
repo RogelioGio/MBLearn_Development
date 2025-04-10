386440f5096f39549df37d41b790bc6c09c91274
RogelioGio <giotalingdan@gmail.com>
2025-04-10 12:42:36 +0800

    feat: add user reactivation component with search and filter functionality

--- MBLearn/src/modalsandprops/SystemSettingComponents.jsx/ReactivationAccountSetting.jsx
+++ MBLearn/src/modalsandprops/SystemSettingComponents.jsx/ReactivationAccountSetting.jsx
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from "react";
 import UserSecEntyProps from "../UserSecEntyProps";
 import axiosClient from "MBLearn/src/axios-client";
 import UserCredentialsLoadingProps from "../UserCredentialsLoadingProps";
+import UserReactivationProps from "./UserReactivationProps";
+import { faFilter, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const ReactivationAccountSetting = () => {
     const {departments, titles, cities, location} = useOption();
@@ -32,15 +36,30 @@ const ReactivationAccountSetting = () => {
     },[])
 
     return(
-        <div className="mx-5 py-5 row-span-2 col-span-3 grid grid-cols-2 grid-rows-[min-content_auto] gap-2">
+        <div className="mx-5 py-5 row-span-2 col-span-3 grid grid-cols-3 grid-rows-[min-content_auto_auto] gap-2">
             {/* Header */}
-            <div className="row-span-1 col-span-2 flex flex-row justify-between items-center">
+            <div className="row-span-1 col-span-3 flex flex-row justify-between items-center">
                 <div>
                     <h1 className="font-header text-primary text-xl">Re-Activation Account Management</h1>
-                    <p className="font-text text-unactive">Manage and list all available inactive employee accounts in the system for reactivation or permanent deletion</p>
+                    <p className="font-text text-unactive text-xs">Manage and list all available inactive employee accounts in the system for reactivation or permanent deletion</p>
                 </div>
             </div>
-            <div className='row-start-2 row-span-2 col-start-1 col-span-4'>
+            <div className="col-start-1 col-span-1 flex flex-col justify-center">
+                <div className="w-1/2 py-2 font-header text-primary flex flex-row gap-2 justify-center items-center border-2 border-primary p-2 rounded-md shadow-md hover: cursor-pointer hover:scale-105 transition-all ease-in-out hover:bg-primaryhover hover:text-white">
+                    <FontAwesomeIcon icon={faFilter}/>
+                    <p>Filter</p>
+                </div>
+            </div>
+            <div className="row-start-2 col-start-3 py-3">
+                <div className=' inline-flex flex-row place-content-between border-2 border-primary rounded-md w-full font-text shadow-md'>
+                    <input type="text" className='focus:outline-none text-sm px-4 w-full rounded-md bg-white' placeholder='Search...'/>
+                    <div className='bg-primary py-2 px-4 text-white'>
+                        <FontAwesomeIcon icon={faSearch}/>
+                    </div>
+                </div>
+            </div>
+            {/* Table */}
+            <div className='row-start-3 row-span-2 col-start-1 col-span-3'>
                 <div className='w-full border-primary border rounded-md overflow-hidden shadow-md'>
                 <table className='text-left min-w-full table-layout-fixed'>
                     <thead className='font-header text-xs text-primary bg-secondaryprimary uppercase'>
@@ -48,31 +67,24 @@ const ReactivationAccountSetting = () => {
                             <th className='p-4 w-2/7'>EMPLOYEE NAME</th>
                             <th className='p-4 w-1/7'>Branch & Location</th>
                             <th className='p-4 w-1/7'>Division & Section</th>
-                            <th className='p-4 w-1/7'>ROLE</th>
                             <th className='p-4 w-1/7'>Last Login Timestamp</th>
-                            <th className='p-4 w-1/7'></th>
                         </tr>
                     </thead>
                     <tbody className='bg-white divide-y divide-divider'>
                         {
                             loading ? (
-                                <UserCredentialsLoadingProps/>
+                                <tr className="font-text text-sm hover:bg-gray-200">
+                                    <td colSpan={4} className="text-center py-3 px-4 font-text text-primary">
+                                        Loading...
+                                    </td>
+                                </tr>
                             ) : (
                                 users.map(user => (
-                                    <UserSecEntyProps
-                                        key={user.id}
-                                        user={user.id}
-                                        name={[user.user_infos?.first_name, user.user_infos?.middle_name, user.user_infos?.last_name].join(" ")}
-                                        employeeID={user.user_infos?.employeeID}
-                                        MBEmail={user.MBemail}
-                                        city={1}
-                                        branch={1}
-                                        department={1}
-                                        title={1}
-                                        role={user.user_infos?.roles?.[0]?.role_name}
-                                        image={user.user_infos?.profile_image }
-                                        status={user.user_infos?.status}
-                                        />
+                                    <UserReactivationProps image={user.user_infos?.profile_image}
+                                    name={[user.user_infos?.first_name, user.user_infos?.middle_name, user.user_infos?.last_name].join(" ")}
+                                    MBEmail={user.MBemail}
+                                    branch={user.user_infos?.branch_id}
+                                    city={user.user_infos?.city_id}/>
                                 ))
                             )
 
@@ -80,6 +92,9 @@ const ReactivationAccountSetting = () => {
                         }
                     </tbody>
                 </table>
+                </div>
+                <div>
+                    Paganiation here
                 </div>
             </div>
         </div>
---
+++ MBLearn/src/modalsandprops/SystemSettingComponents.jsx/UserReactivationProps.jsx
@@ -0,0 +1,38 @@
+import { useOption } from "MBLearn/src/contexts/AddUserOptionProvider"
+
+const UserReactivationProps = ({image, name, MBEmail, branch, city}) => {
+    const {cities,departments,location,titles,roles,permission} = useOption()
+
+    return(
+        <tr className='font-text text-sm hover:bg-gray-200 hover:cursor-pointer'>
+            <td className='text-sm py-3 px-4'>
+                <div className='flex items-center gap-2'>
+                    {/* User Image */}
+                    <div className='bg-blue-500 h-10 w-10 rounded-full'>
+                        <img alt="" src={image} className='rounded-full'/>
+                    </div>
+                    {/* Name */}
+                    <div>
+                        <p className='font-text'>{name}</p>
+                        <p className='text-unactive'>{MBEmail}</p>
+                    </div>
+                </div>
+            </td>
+            <td className='py-3 px-4'>
+                <div className='flex flex-col'>
+                    {/* Branch Location */}
+                    <p className='text-unactive'>{location?.find(l => l.id === branch).branch_name}</p>
+                    {/* City Location */}
+                    <p className='text-unactive text-xs'>{cities?.find(c => c.id === city)?.city_name}</p>
+                </div>
+            </td>
+            <td className='py-3 px-4'>
+
+            </td>
+            <td className='py-3 px-4'>
+
+            </td>
+        </tr>
+    )
+}
+export default UserReactivationProps
