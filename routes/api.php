67836b08d6157b3d2f8857ddb3fea93e288a9836
Jericho Ilanga <ilangajericho@gmail.com>
2025-05-13 14:10:51 +0800

    abc

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/user', function (Request $request) {
         return $request->user()->load(['userInfos', 'userInfos.permissions','userInfos.roles',]);
     });
-    Route::get('/status/{userId}/{lessonId}', function (Request $request, UserInfos $userId, Lesson $lessonId) {
+    Route::get('/status/{userId}/{lessonId}', function (Request $request, UserInfos $userId, $lessonId) {
         return response()->json(['status' => 'complete']);
     });
     //test purposes amd account implementation (postman testing)
