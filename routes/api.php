8711454b059d22da463080e29108c9e7d4b7b77a
Jericho Ilanga <ilangajericho@gmail.com>
2025-05-14 16:49:44 +0800

    course context for admin

--- routes/api.php
+++ routes/api.php
@@ -135,6 +135,7 @@ Route::middleware('auth:sanctum')->group(function(){
     //Fetching All nessesary call for courselist maintenance
     Route::get('/coursecontext',[CourseContextController::class,'index']);
     Route::get('/coursecontext/{id}/{userInfos}',[CourseContextController::class,'getSelectedCourse']);
+    Route::get('/coursecontext/{id}',[CourseContextController::class,'adminGetSelectedCourse']);
 
 
     //extra (permission)
