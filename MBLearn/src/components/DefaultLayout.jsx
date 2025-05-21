982eba092592b10ac9f5b5d5b3c4eab80c153867
RogelioGio <giotalingdan@gmail.com>
2025-05-21 23:10:55 +0800

    Add password setup form with validation criteria in DefaultLayout

--- MBLearn/src/components/DefaultLayout.jsx
+++ MBLearn/src/components/DefaultLayout.jsx
@@ -8,6 +8,9 @@ import { use } from 'react';
 import LogoutWarningmModal from '../modalsandprops/LogoutWarningModal';
 import { SelectedUserProvider } from '../contexts/selecteduserContext';
 import { OptionProvider } from '../contexts/AddUserOptionProvider';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useFormik } from 'formik';
 
 export default function DefaultLayout() {
     const {token, role, setRole, setUser, setProfile} = useStateContext();
@@ -30,6 +33,11 @@ export default function DefaultLayout() {
             localStorage.setItem('LAST_ACTIVITY', Date.now());
         }
     };
+
+    // Formik
+    const formik = useFormik({
+
+    })
 
     // useEffect(() => {
     //     if(!token) return;
@@ -106,7 +114,74 @@ export default function DefaultLayout() {
         return <Navigate to="/login" replace/>
     }
 
+    if(true){
+        return(
+        <div className='h-screen w-screen flex flex-col justify-center items-center bg-background gap-3'>
+            <div className='flex flex-col w-[50vw] bg-white rounded-lg shadow-lg p-5'>
+                {/* Headder */}
+                <div className="pt-2 pb-4 mx-4 border-b border-divider flex flex-row gap-4 col-span-2">
+                    <div>
+                        <h1 className="text-primary font-header text-3xl">Set Password</h1>
+                        <p className="text-unactive font-text text-xs">Replace their auto-generated initial password with a secure, personalized password during first-time login for enhanced account security.</p>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <div className="h-fit bg-primarybg p-5 rounded-full flex items-center justify-center">
+                            <div className="h-full w-fit aspect-square flex items-center justify-center">
+                                <FontAwesomeIcon icon={faClock} className="text-primary text-lg"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Password */}
+                <form className='py-2 px-5 w-full'>
+                    <div className="inline-flex flex-col w-full">
+                        <label htmlFor="course_name" className="font-header text-xs flex flex-row justify-between">
+                            <p className="font-text text-unactive">New Password:</p>
+                        </label>
+                        <input type="text" name="course_name"
+                            // value={formik2.values.course_name}
+                            // onChange={formik2.handleChange}
+                            // onBlur={formik2.handleBlur}
+                            // disabled = {fetchedCourse?.CourseName}
+                            className="font-text border border-divider rounded-md p-2 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-primary"/>
+                            {/* {formik2.touched.course_name && formik2.errors.course_name ? (<div className="text-red-500 text-xs font-text">{formik2.errors.course_name}</div>):null} */}
+                    </div>
+                    {/* Password Criteria */}
+                    <div>
+                        <p>Password Criteria:</p>
+                        <ul>
+                            <li>At least 8 characters</li>
+                            <li>At least one uppercase letter</li>
+                            <li>At least one lowercase letter</li>
+                            <li>At least one number</li>
+                            <li>At least one special character</li>
+                        </ul>
+                    </div>
+                    <div className="inline-flex flex-col w-full">
+                        <label htmlFor="course_name" className="font-header text-xs flex flex-row justify-between">
+                            <p className="font-text text-unactive">Confirm Password:</p>
+                        </label>
+                        <input type="text" name="course_name"
+                            // value={formik2.values.course_name}
+                            // onChange={formik2.handleChange}
+                            // onBlur={formik2.handleBlur}
+                            // disabled = {fetchedCourse?.CourseName}
+                            className="font-text border border-divider rounded-md p-2 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-primary"/>
+                            {/* {formik2.touched.course_name && formik2.errors.course_name ? (<div className="text-red-500 text-xs font-text">{formik2.errors.course_name}</div>):null} */}
+                    </div>
+                    <div>
+                        submit
+                    </div>
+                </form>
+            </div>
+        </div>
+        )
+    }
+
     if(loading){
+
+
+
         if(!token){
             return <Navigate to="/login" replace/>
         }
